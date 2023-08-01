61fdc1b0f7adc47b3e06234190254b5c2ef29e5c
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-08-01 21:54:58 +0530

    Task - 1 basic node server

---
+++ Task-1(Basic Node server)/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors")
+require("dotenv").config();
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.use("/",async(req,res)=>{
+    res.status(200).send("Hello, World!")
+})
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,async()=>{
+    console.log(`Server is running`);
+})
